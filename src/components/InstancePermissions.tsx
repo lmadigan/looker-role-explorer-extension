f1be384139a0a059505cbec149a6b4974870529f
Lauren Madigan <lauren.madigan@looker.com>
2019-12-04 15:07:42 -0800

    Lara's changesgit add -A

--- src/components/InstancePermissions.tsx
+++ src/components/InstancePermissions.tsx
@@ -1,76 +1,103 @@
-import React from "react"
-import { IPermission, IModelSet, IRole, } from "@looker/sdk"
-import { Flex, Box, Heading, Text, Paragraph } from '@looker/components'
-import { ExtensionContext } from "../framework/ExtensionWrapper"
-import { getInstancePermissions, distinct } from '../util/permissions'
-import { limitByRadius } from "@looker/components/dist/types/Form/Fields/FieldColor/ColorWheel/math_utils"
-
+import React from "react";
+import { IPermission, IModelSet, IRole } from "@looker/sdk";
+import { Flex, Box, Heading, Text, Paragraph } from "@looker/components";
+import { ExtensionContext } from "../framework/ExtensionWrapper";
+import {
+  getModelPermissions,
+  getInstancePermissions,
+  getUniquePermissions,
+  distinct
+} from "../util/permissions";
 interface InstancePermissionProps {
-  roles?: IRole[]
+  roles?: IRole[];
 }
-
 interface InstancePermissionState {
-  instancePermissions: string[]
+  instancePermissions: Record<string, string>[];
 }
-
-class  InstancePermissions extends React.Component<InstancePermissionProps, InstancePermissionState> {
+class InstancePermissions extends React.Component<
+  InstancePermissionProps,
+  InstancePermissionState
+> {
   constructor(props: InstancePermissionProps) {
-    super(props)
+    super(props);
     this.state = {
       instancePermissions: []
-    }
+    };
   }
-
   componentDidMount() {
-    const instancePermissiosn = this.getInstancePermissions()
+    const instancePermissions = this.getInstancePermissions();
     this.setState({
-      instancePermissions: instancePermissiosn
-    })
+      instancePermissions: instancePermissions
+    });
   }
- 
-  componentDidUpdate(prevProps: InstancePermissionProps, prevState: InstancePermissionState) {
-    if ( prevProps.roles !== this.props.roles ) {
-      const instancePermissiosn = this.getInstancePermissions()
-      console.log(instancePermissiosn)
+  componentDidUpdate(
+    prevProps: InstancePermissionProps,
+    prevState: InstancePermissionState
+  ) {
+    if (prevProps.roles !== this.props.roles) {
+      const instancePermissions = this.getInstancePermissions();
       this.setState({
-        instancePermissions: instancePermissiosn
-      })
+        instancePermissions: instancePermissions
+      });
     }
   }
- 
   getInstancePermissions() {
-    const { roles } = this.props 
-    let newPermissions: string[] = []
-    if ( roles ) {
-      for ( let role of roles ) {
-        const instancePerms = this.getInstancePermissionsForRole(role)
-        newPermissions = instancePerms.concat(newPermissions).filter(distinct)
+    const { roles } = this.props;
+    let newPermissions: Record<string, string>[] = [];
+    if (roles) {
+      for (let role of roles) {
+        const instancePerms = this.getInstancePermissionsForRole(role);
+        newPermissions = instancePerms.concat(newPermissions).filter(distinct);
       }
     }
-    return newPermissions
+    return newPermissions;
   }
-
   getInstancePermissionsForRole(role: IRole) {
-    let permissions: string[] = []
+    let permissions: Record<string, string>[] = [];
     if (role.permission_set && role.permission_set.permissions) {
-      permissions = getInstancePermissions(role.permission_set.permissions)
-    } 
-    return permissions
+      permissions = getInstancePermissions(role.permission_set.permissions);
+    }
+    return permissions;
   }
-
+  eachCategories = (list: any) =>
+    list.map((permission: any, index: any) => (
+      <Paragraph key={index} fontSize="small">
+        {permission}
+      </Paragraph>
+    ));
+  instancePermissionsList = (instancePermissions: any) => {
+    const finalList: any = {};
+    instancePermissions.map((permission: Record<string, string>) => {
+      if (finalList[permission.category]) {
+        finalList[permission.category].push(permission.value);
+      } else {
+        finalList[permission.category] = [permission.value];
+      }
+    });
+    let arr = [];
+    for (const category in finalList) {
+      arr.push(
+        <div key={category}>
+          <Paragraph fontWeight="bold" fontSize="medium">
+            {category}
+          </Paragraph>
+          {this.eachCategories(finalList[category])}
+        </div>
+      );
+    }
+    return arr;
+  };
   render() {
-    const { instancePermissions } = this.state
+    const { instancePermissions } = this.state;
+    const list: any = this.instancePermissionsList(instancePermissions);
     return (
-      <Flex flexDirection='column' mr='xxxlarge'>
-        <Paragraph fontSize='medium' mb='large' color='palette.charcoal600'>Instance Permissions</Paragraph>
-        {
-          instancePermissions.map((perm: string, ind: string | number | undefined) => {
-          return <Paragraph key={ind} fontSize='small'>{perm}</Paragraph>
-          })
-        }
-      </Flex> 
-    )
+      <Flex flexDirection="column" mr="xxxlarge">
+        <Paragraph fontSize="medium" mb="large" color="palette.charcoal600">
+          Instance Permissions
+        </Paragraph>
+        {list}
+      </Flex>
+    );
   }
 }
-
 export default InstancePermissions;
--- src/util/permissions.ts
+++ src/util/permissions.ts
@@ -1,69 +1,192 @@
 const instancePermissions = [
-  'create_prefetches',
-  'embed_browse_spaces',
-  'login_special_email',
-  'manage_homepage',
-  'manage_models',
-  'manage_spaces',
-  'see_logs',
-  'see_queries',
-  'see_schedules',
-  'see_system_activity',
-  'see_users',
-  'sudo',
-  'support_access_toggle',
-]
-
+  {
+    value: "create_prefetches",
+    category: "Data Access"
+  },
+  {
+    value: "embed_browse_spaces",
+    category: "BI Interaction"
+  },
+  {
+    value: "login_special_email",
+    category: "Data Access"
+  },
+  {
+    value: "manage_homepage",
+    category: "BI Interaction"
+  },
+  {
+    value: "manage_models",
+    category: "Development Tools"
+  },
+  {
+    value: "manage_spaces",
+    category: "BI Interaction"
+  },
+  {
+    value: "see_logs",
+    category: "Admin"
+  },
+  {
+    value: "see_queries",
+    category: "Admin"
+  },
+  {
+    value: "see_schedules",
+    category: "Admin"
+  },
+  {
+    value: "see_system_activity",
+    category: "Admin"
+  },
+  {
+    value: "see_users",
+    category: "Admin"
+  },
+  {
+    value: "sudo",
+    category: "Admin"
+  },
+  {
+    value: "support_access_toggle",
+    category: "Admin"
+  }
+];
 const allAccessibleModels = [
-  'see_pdts',
-  'see_datagroups',
-  'update_datagroups',
-  'create_alerts',
-  'follow_alerts',
-  'send_outgoing_webhook',
-  'send_to_s3',
-  'send_to_sftp',
-  'send_to_integration',
-  'create_table_calculations',
-  'save_content',
-  'schedule_external_look_emails',
-  'deploy',
-]
-
+  {
+    value: "see_pdts",
+    category: "Admin"
+  },
+  {
+    value: "see_datagroups",
+    category: "Admin"
+  },
+  {
+    value: "update_datagroups",
+    category: "Admin"
+  },
+  {
+    value: "create_alerts",
+    category: "BI Interaction"
+  },
+  {
+    value: "follow_alerts",
+    category: "BI Interaction"
+  },
+  {
+    value: "send_outgoing_webhook",
+    category: "Data Access"
+  },
+  {
+    value: "send_to_s3",
+    category: "Data Access"
+  },
+  {
+    value: "send_to_sftp",
+    category: "Data Access"
+  },
+  {
+    value: "send_to_integration",
+    category: "Data Access"
+  },
+  {
+    value: "create_table_calculations",
+    category: "Data Access"
+  },
+  {
+    value: "save_content",
+    category: "BI Interaction"
+  },
+  {
+    value: "schedule_external_look_emails",
+    category: "Data Access"
+  },
+  {
+    value: "deploy",
+    category: "Development Tools"
+  }
+];
 const modelPermissions = [
-  'access_data',
-  'see_drill_overlay',
-  'see_lookml_dashboards',
-  'see_looks',
-  'download_with_limit',
-  'download_without_limit',
-  'see_user_dashboards',
-  'see_sql',
-  'explore',
-  'create_public_looks',
-  'schedule_look_emails',
-  'see_lookml',
-  'use_sql_runner',
-  'develop'
-]
-
+  {
+    value: "access_data",
+    category: "Data Access"
+  },
+  {
+    value: "see_drill_overlay",
+    category: "BI Interaction"
+  },
+  {
+    value: "see_lookml_dashboards",
+    category: "BI Interaction"
+  },
+  {
+    value: "see_looks",
+    category: "BI Interaction"
+  },
+  {
+    value: "download_with_limit",
+    category: "Data Access"
+  },
+  {
+    value: "download_without_limit",
+    category: "Data Access"
+  },
+  {
+    value: "see_user_dashboards",
+    category: "BI Interaction"
+  },
+  {
+    value: "see_sql",
+    category: "BI Interaction"
+  },
+  {
+    value: "explore",
+    category: "BI Interaction"
+  },
+  {
+    value: "create_public_looks",
+    category: "Data Access"
+  },
+  {
+    value: "schedule_look_emails",
+    category: "Data Access"
+  },
+  {
+    value: "see_lookml",
+    category: "Development Tools"
+  },
+  {
+    value: "use_sql_runner",
+    category: "Development Tools"
+  },
+  {
+    value: "develop",
+    category: "Development Tools"
+  }
+];
 export const distinct = (value: any, index: any, self: any) => {
   return self.indexOf(value) === index;
+};
+export function getUniquePermissions(
+  permissionList1: string[],
+  permissionList2: Record<string, string>[]
+) {
+  let intersection = [];
+  for (let x of permissionList1) {
+    for (let y of permissionList2) {
+      if (x === y.value) {
+        intersection.push(y);
+      }
+    }
+  }
+  return intersection;
 }
-
-export function getUniquePermissions(permissionList1: string[], permissionList2: string[]) {
-  let intersection = permissionList1.filter(x => permissionList2.includes(x));
-  return intersection
-}
-
 export function getModelPermissions(permissionList: string[]) {
-  return getUniquePermissions(permissionList, modelPermissions)
+  return getUniquePermissions(permissionList, modelPermissions);
 }
-
 export function getInstancePermissions(permissionList: string[]) {
-  return getUniquePermissions(permissionList, instancePermissions)
+  return getUniquePermissions(permissionList, instancePermissions);
 }
-
 export function getAllAccessibleModelPermissions(permissionList: string[]) {
-  return getUniquePermissions(permissionList, allAccessibleModels)
+  return getUniquePermissions(permissionList, allAccessibleModels);
 }
